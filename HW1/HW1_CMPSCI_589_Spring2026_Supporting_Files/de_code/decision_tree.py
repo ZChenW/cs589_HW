8d3fc301f6023d3b26795575f1071663e9ea3a34
CHAKEW <zichenw@umass.edu>
2026-02-18 20:11:57 -0500

    getTree()

--- HW1/HW1_CMPSCI_589_Spring2026_Supporting_Files/de_code/decision_tree.py
+++ HW1/HW1_CMPSCI_589_Spring2026_Supporting_Files/de_code/decision_tree.py
@@ -12,7 +12,7 @@ class DecisionTree:
         y_label = data.iloc[:, -1]
         info = 0
         enp = y_label.value_counts().values / len(y_label)
-        info -= -enp * (np.log2(enp))
+        info += -enp * (np.log2(enp))
         return np.sum(info)
 
     ## Cal Gain with attribute
@@ -45,7 +45,26 @@ class DecisionTree:
         update_new = [(i[0], i[1].drop([bestfeature], axis=1)) for i in new_data]
         return update_new
 
-    def SpliteNond(self, data):
-        beforesplite = self.InformationGain(data)
-        best_feature_index = -1
-        beat_info_enp = 0
+    def get_most_label(self, data):
+        labels = data.iloc[:, -1]
+        label_sort = labels.value_counts(sort=True)
+        return label_sort.keys()[0]
+
+    def getTree(self, data):
+        ## 停止条件：1. attibute的标签全为一类 2. 样本只剩下一个特征
+        lables = data.iloc[0, -1].value_counts()  # biaoqian
+        column = list(data.columns)  # tezheng
+
+        if lables[0] == len(data) or len(column) == 1:
+            return lables[0]
+
+        bestfeature = self.GetBestFeature(data)
+        col = column[bestfeature]
+        tree = {col: {}}
+        del column[bestfeature]
+
+        for i in set(data[col]):
+            update_new = self.SpliteByFeature(data, bestfeature)
+            tree[col][i] = self.getTree(update_new)
+
+        return tree
